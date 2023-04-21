ab6a352f6e1af267c2e3a82a5e3afe335ad5a829
Magnus Holmen <themagnus1208@gmail.com>
2023-04-21 10:35:23 +0200

    Exploring

--- src/main/java/org/Oauth/Controller.java
+++ src/main/java/org/Oauth/Controller.java
@@ -1,9 +1,13 @@
 package org.Oauth;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -20,6 +24,11 @@ public class Controller {
         int i = (int)counter.incrementAndGet();
 
         return new Response(i,melding);
+    }
 
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        return "/";
     }
 }
